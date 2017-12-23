836ae9b1b1e5fc3fce2b075a439218d0104473e1
Dominique Louis <savagesoftware@gmail.com>
2017-12-23 01:43:10 +0100

    [Xamarin.Mac]Add Xamarin.Mac version of MacOS project, with content pipeline project.

---
+++ LD27.Xamarin.Mac/Main.cs
@@ -0,0 +1,26 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using AppKit;
+using Foundation;
+#endregion
+
+namespace LD27.Xamarin.Mac
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main (string[] args)
+		{
+			NSApplication.Init ();
+
+			using (var game = new LD27Game ()) {
+				game.Run ();
+			}
+		}
+	}
+}
